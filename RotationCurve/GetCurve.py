4bd4c8a275f02a8fe4649517cedc4865215b96fa
killerkaustubh <kaustubh.waghmare@gmail.com>
2014-12-09 19:24:45 +0530

    Following changes to GetCurve.py 	- Now reads parameters from a configuration file. 	- Also accepts error frames. 	- Uses lmfit to do all Gaussian fitting 	- Can fit multiple Gaussians simultaneously. 	- Object RA, DEC and JD information now computed from the header. RotationCurveLib.py completely modified. Lots of new functions added.

--- RotationCurve/GetCurve.py
+++ RotationCurve/GetCurve.py
@@ -1,44 +1,54 @@
+"""
+Usage: python GetCurve.py <2d_spectrum_file> <2d_error_file>
+Displays central spectrum.
+Allows you to mark wavelengths using m.
+Goes and fits Gaussians across the whole range.
+"""
+
 import sys
 import matplotlib.pyplot as plt
-import numpy as np
-import pyfits as pf
-#from GetCurve_Config import *
-from RotationCurveLib import *
-import pywcs as pw
-import scipy.optimize as so
-from Fitting import *
-from helcorr import *
+from MatplotlibCustom import *
+from RotationCurveLib2 import *
+from helcorr import helcorr
 from astropy.table import Table, Column
-import os
+import pickle
+
+config_file = "GetCurve.config"
 
 # Parse command line arguments.
 try:
 	spectrum_file = sys.argv[1]
 except:
 	print("Insufficient number of arguments.")
-	print("Usage: python GetCurve.py <2dspec_file>")
+	print("Usage: python GetCurve.py <2dspec_file> <2d_specErr>")
 	sys.exit(2)
 
-# Check if file exists, else throw error.
 try:
-	hdulist = pf.open(spectrum_file)
+	error_file = sys.argv[2]
 except:
-	print "File does not exist or corrupt."
-	sys.exit(3)
+	error_file = 0
 
-# Load the file, get data and extract wavelengths using WCS embedded info.
-# Parse WCS information
-header = hdulist[0].header
-data = hdulist[0].data
-wcs = pw.WCS(header)
-wavelengths =  wcs.wcs_pix2sky( [ [i+0.5,1] for i in range( header["NAXIS1"] )] ,1 )
-wavelengths = [ wavelengths[i][0] for i in range(len(wavelengths)) ]
+# Check if files can be loaded with a straight face!
+header, data, wavelengths = read_spectrum( spectrum_file )
+if header == -1:
+	print("File corrupt or does not exist.")
+	sys.exit(3)
+if error_file:
+	er_header, er_data, er_wavelengths = read_spectrum( error_file )
+	if er_header == -1:
+		print("Error file corrupt or does not exist.")
+		sys.exit(4)
+# DONE! Files now loaded.
+
+#############################################
+#############################################
+# To display the spectrum for spatial centre.
+#############################################
+#############################################
 
 # Determine the centroid row for displaying a spectrum.
-
 centroid =  GetCentroid( range(header["NAXIS2"]), np.sum(data, axis=1) )
 central_row = int(round(centroid, 0))
-
 
 # Display the file plot and enable input from the user.
 fig1 = plt.figure(1)
@@ -51,42 +61,51 @@ plt.grid()
 c = Coordinate(fig1)
 plt.show()
 dumb_input = raw_input()
-rest_wavelength = float(raw_input("Enter Rest Wavelength for Selected Feature: "))
-
-
 x1, x2 = c.getcoords()
-# To check if user entered coordinates in the right order.
-if x1 > x2:
-	x1,x2 = x2,x1
 
-# Convert wavelength coordinates to pixels since we are dealing with those.
-p1, p2 = Wavelength2Pixels(wcs, x1, x2)
+# Prompt the user for number of Gaussian components needed to model the selected spectrum.
+no_components = int(raw_input("Enter number of Gaussian Components to model: "))
+
+# Prompt the user for rest wavelengths.
+rest_wavelengths = np.ones(no_components)
+for i in range(no_components):
+	rest_wavelengths[i] = float(raw_input("Enter Rest Wavelength for Selected Feature(s): "))
+
+########################################################
+########################################################
+# Rotation Curve Extraction
+########################################################
+########################################################
+
+# First off, we need to access data from the spectrum but for user
+# convenience we have dealt with wavelengths. Let's get the pixels, shall we?
+p1, p2 = Wavelength2Pixels(header, x1, x2)
 p1, p2 = np.floor(p1), np.ceil(p2)
 
-
-# Define some parameters. To be later moved into a separate config file.
-stepsize = 5
-nsum = 5
-uppercut = 100
-pixel_scale = 0.1 # arcsec per pixel	
-speed_light = 3e5
+# Read configuration file.
+stepsize, nsum, uppercut, pixel_scale, speed_light = CurveParams(config_file)
 
 # Create an array of pixels over which we will map the spectral feature.
 # This, converted into wavelengths will serve as x in the Gaussian fit.
 # Note that 0.5 addition in the xvar = .. step is because we want to take
 # the wavelength for the mid-point of the pixel.
 pixels = np.arange(p1, p2, 1)
-xvar = Pixels2Wavelengths(wcs, *(pixels+0.5) )
+xvar = Pixels2Wavelengths(header, *(pixels+0.5) )
 xvar = np.array(xvar)
 
+# Initialize empty lists to store centroids and error bars in them.
 centroids = [] 
 centroid_errs = []
 spatial_points = []
 for aperture in range(central_row, central_row + uppercut, stepsize):
 	plt.plot( np.linspace(x1, x2, p2-p1),  np.mean(data[aperture:aperture+nsum,p1:p2], axis=0) , label="%d" % aperture )
-	yvar = np.mean(data[aperture:aperture+nsum,p1:p2], axis=0) 
+	yvar = np.mean(data[aperture:aperture+nsum,p1:p2], axis=0)
+	if error_file:
+		yvar_err = np.mean(er_data[aperture:aperture+nsum,p1:p2], axis=0)
+	else:
+		yvar_err = None
 	try:
-		c, cerr = GaussFit(xvar, yvar)
+		c, cerr = FitGaussians(xvar, yvar, yvar_err, no_components)
 		centroids.append(c)
 		centroid_errs.append(cerr)
 		spatial_points.append( aperture + float(stepsize)/2.0 )
@@ -96,65 +115,74 @@ for aperture in range(central_row, central_row + uppercut, stepsize):
 for aperture in range(central_row, central_row - uppercut, -1*stepsize):
 	plt.plot( np.linspace(x1, x2, p2-p1),  np.mean(data[aperture:aperture+nsum,p1:p2], axis=0) , label="%d" % aperture )
 	yvar = np.mean(data[aperture:aperture+nsum,p1:p2], axis=0) 
+	if error_file:
+		yvar_err = np.mean(er_data[aperture:aperture+nsum,p1:p2], axis=0)
+	else:
+		yvar_err = None
 	try:
-		c, cerr = GaussFit(xvar, yvar)
+		c, cerr = FitGaussians(xvar, yvar, yvar_err, no_components)
 		centroids.append(c)
 		centroid_errs.append(cerr)
 		spatial_points.append( aperture + float(stepsize)/2.0 )
 	except:
 		pass
 
+
 # Convert spatial row numbers to arc second distance from centre.
 spatial_points_arc = (spatial_points - centroid)*pixel_scale
 
 # Convert wavelength centroids into redshifts.
-centroids_redshifts = (np.array(centroids) - rest_wavelength)/rest_wavelength
+centroids = np.array(centroids)
+centroid_errs = np.array(centroid_errs)
+centroids_redshifts = (centroids - rest_wavelengths)/rest_wavelengths
 # Compute errors on redshifts.
-centroids_redshifts_err = np.sqrt( (1.0/rest_wavelength)**2*np.array(centroid_errs)**2 )
+centroids_redshifts_err = np.sqrt( (1.0/rest_wavelengths)**2*np.array(centroid_errs)**2 )
 # Convert wavelength centroids into km/s using rest-wavelength comparison. 
-centroids_vel = (np.array(centroids) - rest_wavelength)/rest_wavelength * speed_light
+centroids_vel = (np.array(centroids) - rest_wavelengths)/rest_wavelengths * speed_light
+
 # Compute heliocentric correction.
-correction, hjd = helcorr(-20.810678, -32.376006, 1798, 4.269583, -55.780139, 2455084.537578)
+
+# First, get observatory information.
+lon, lat, alt = ObservatoryInformation(config_file)
+# Then, get object coordinates from input file.
+ra, dec, jd = ObjectInformation(header)
+
+correction, hjd = helcorr(lon, lat, alt, ra, dec, 2455084.537578)
 # Apply heliocentric correction.
 centroids_vel_helio = centroids_vel + correction
 # Transform error bars accordingly.
-centroids_vel_helio_err = np.sqrt( (speed_light/rest_wavelength)**2*np.array(centroid_errs)**2 + 1e-6)
+centroids_vel_helio_err = np.sqrt( (speed_light/rest_wavelengths)**2*np.array(centroid_errs)**2 + 1e-6)
 
 
-# Plot rotation curve.
+# Plot rotation curve / curves.
 fig2 = plt.figure(2)
-plt.errorbar( spatial_points_arc, centroids_vel_helio, centroids_vel_helio_err, fmt="o" )
 plt.xlabel("Distance from Center [arcsec]", fontsize=18)
 plt.ylabel("Heliocentric Radial Velocity [km/s]", fontsize=18)
+for i in range(no_components):
+	plt.errorbar( spatial_points_arc, centroids_vel_helio[:,i], centroids_vel_helio_err[:,i], fmt="o" )
+	plt.show()
+	raw_input()
+	plt.clf()
 
-# Output all rotation cruve data.
-out = Table()
-out.add_column( Column(spatial_points, name="Row") )
-out.add_column( Column(spatial_points_arc, name="DFCG") )
-out.add_column( Column(centroids, name="lambda") )
-out.add_column( Column(centroid_errs, name="lambda_err") )
-out.add_column( Column(centroids_vel, name="V") )
-out.add_column( Column(centroids_vel_helio, name="Vhel") )
-out.add_column( Column(centroids_vel_helio_err, name="Vhel_rr") )
-out.add_column( Column(centroids_redshifts, name="z") )
-out.add_column( Column(centroids_redshifts_err, name="z_err") )
-
-# Ask user for file name to store rotation data.
-filename = raw_input("Enter File Name to Save Data: ").rstrip()
-out.write(filename, format="ascii", delimiter="\t")
-
-# Display plot.
-plt.grid()
-fig2.show()
-dum = raw_input()
-
-
-
-
-
-
-
-
-
-
-
+continue_flag = input_str("Satisfied with at least one rotation curve? (y/n): ")
+if continue_flag == "y":
+	# Ask user for file name to store rotation data.
+	filename = raw_input("Enter Base File Name to Save Data: ").rstrip()
+	# Output all rotation curve data.
+	for i in range(no_components):
+		out = Table()
+		out.add_column( Column(spatial_points, name="Row") )
+		out.add_column( Column(spatial_points_arc, name="DFCG") )
+		out.add_column( Column(centroids[:,i], name="lambda") )
+		out.add_column( Column(centroid_errs[:,i], name="lambda_err") )
+		out.add_column( Column(centroids_vel[:,i], name="V") )
+		out.add_column( Column(centroids_vel_helio[:,i], name="Vhel") )
+		out.add_column( Column(centroids_vel_helio_err[:,i], name="Vhel_rr") )
+		out.add_column( Column(centroids_redshifts[:,i], name="z") )
+		out.add_column( Column(centroids_redshifts_err[:,i], name="z_err") )
+	
+		out.write(filename+"_%d.dat" % (i+1), format="ascii", delimiter="\t")
+	
+	rest_file = open(filename+"_rest.out","w")
+	pickle.dump(rest_wavelengths, rest_file)
+	rest_file.close()
--- RotationCurve/RotationCurveLib.py
+++ RotationCurve/RotationCurveLib.py
@@ -1,21 +1,45 @@
-import numpy as np
+"""
+A collection of functions to be used by GetCurve2.py in order to 
+perform various operations on it.
+"""
+
+from astropy.io import fits
+import astropy.wcs as pw
 import matplotlib.pyplot as plt
-from Fitting import *
+import numpy as np
+from lmfit.models import GaussianModel, ConstantModel
+import string
+import ConfigParser
+from astropy import time
 
+# Define a simple function to read a 2-d spectrum.
+def read_spectrum(filename):
+	"""
+	Input: Name of the file.
+	Output: header object, data object, wavelengths for pixel centres.
+		In case the loading of the file fails, you get a -1,-1, -1 tuple.
+	"""
+	try:
+		hdulist = fits.open(filename)
+	except:
+		return -1,-1,-1
+	header = hdulist[0].header
+	data = hdulist[0].data
+	wcs = pw.WCS(header)
+	wavelengths =  wcs.wcs_pix2world( [ [i+0.5,1] for i in range( header["NAXIS1"] )] ,1 )
+	wavelengths = [ wavelengths[i][0] for i in range(len(wavelengths)) ]
 
-# Define a centroiding algorithm, use it get a central row.
-def GetCentroid( spatial_points, intensities):
-	return np.sum(spatial_points * intensities)/np.sum(intensities)
+	return header, data, wavelengths
 
-# # Define an event for accepting input coordinates via clicking.
+# A Matplotlib Connection Event Object to query the coordinates of plots.
 class Coordinate:
 	def __init__(self, figure):
 		self.x = []
 		self.figure = figure
-		figure.canvas.mpl_connect("button_press_event", self)
+		figure.canvas.mpl_connect("key_press_event", self)
 
 	def __call__(self, event):
-		if len(self.x) < 2:
+		if len(self.x) < 2 and event.key == "m":
 			self.x.append( event.xdata )
 			print "Coordinate %d added = %.2f" % (len(self.x), event.xdata)
 		if len(self.x) == 2:
@@ -23,29 +47,128 @@ class Coordinate:
 			plt.close(self.figure)
 
 	def getcoords(self):
-		return self.x
+		if self.x[0] < self.x[1]:
+			return self.x
+		else:
+			return self.x[::-1]
 
-# Define simple functions to convert pixels to wavelength and vice versa.
-def Wavelength2Pixels(wcs, *args):
-	coords = [ [args[i],1] for i in range(len(args)) ]
-	pixels = wcs.wcs_sky2pix( coords, 1)
-	pixels = [ pixels[i][0] for i in range(len(pixels)) ]
-	return pixels
 
-def Pixels2Wavelengths(wcs, *args):
+# Define a centroiding algorithm, use it get a central row.
+def GetCentroid( spatial_points, intensities):
+	return np.sum(spatial_points * intensities)/np.sum(intensities)
+
+def Pixels2Wavelengths(header, *args):
+	wcs = pw.WCS(header)
 	pixels = [ [args[i]+0.5,1] for i in range(len(args)) ]
-	coords = wcs.wcs_pix2sky( pixels, 1)
+	coords = wcs.wcs_pix2world( pixels, 1)
 	coords = [ coords[i][0] for i in range(len(pixels)) ]
 	return coords
 
-# We will fit profiles with Gaussian, so define a fitting function for that.
-def GaussFit(x, y, yerr=None, p0=None):
-	def gaussian(x, a, mu, sigma, offset):
-		return a*np.exp((x-mu)**2/(-2*sigma**2) ) + offset
+def Wavelength2Pixels(header, *args):
+	wcs = pw.WCS(header)
+	coords = [ [args[i],1] for i in range(len(args)) ]
+	pixels = wcs.wcs_world2pix( coords, 1)
+	pixels = [ pixels[i][0] for i in range(len(pixels)) ]
+	return pixels
 
-	if p0 is None:
-		p0 = [ np.sum(y), np.mean(x), np.std(x), np.max(y)-np.min(y) ]
+def FitGaussians(x, y, y_err, num):
+	"""
+	Input: x, generally wavelengths.
+	       y, generally flux
+		   y_err, errors on y
+		   num: number of Gaussians to fit
+	Output: Mean Centroid, Error on Mean Centroid
+	"""
+	# First, define a model.
+	mod = GaussianModel(prefix="g1_")
+	components = 1
+	while components != num:
+		mod = mod + GaussianModel(prefix="g%d_" % (components+1))
+		components += 1
+	mod = mod + ConstantModel()
+	pars = mod.make_params()
 
-	gau_params, gau_unc, rchi2, dof= general_fit( gaussian, x, y, p0, yerr)
-	return gau_params[1], gau_unc[1]
+	# Some initial estimates.
+	bins_on_x = np.linspace(x.min(),x.max(),num+1)
+	x_digitized = np.digitize(x,bins_on_x)
+	
+	for i in range(num):
+		prefix = "g%d_" % (i+1)
+		guess_center = np.mean(x[x_digitized == (i+1)])
+		pars[prefix+"center"].set(guess_center, min=x.min(), max=x.max())
+		pars[prefix+"amplitude"].set(np.sum(y)/num)
+		pars[prefix+"sigma"].set(5)
+		pars["c"].set(np.mean(y))
 
+	# Great, model has been defined, parameters defined. 
+	results = mod.fit(y, pars, x=x, weights=1/y_err)
+	params = results.params
+	centers = np.ones(num, dtype=float)
+	centers_err = np.ones(num, dtype=float)
+	for i in range(num):
+		param_name = "g%d_center" % (i+1)
+		centers[i] = params[param_name].value
+		centers_err[i] = params[param_name].stderr
+	
+	ind_sort = np.argsort(centers)
+	return centers, centers_err
+
+# A basic y/n questioner.
+def input_str(script):
+	while True:
+		userInput = str(raw_input('%s' % (script)))
+		if len(userInput) == 1:
+			if userInput in string.letters:
+				if userInput.lower() == 'y' or userInput.lower() == 'n':		
+					break
+				print 'Please enter only "y" or "n"'
+			else:
+				print 'Please enter only letters!'
+		elif len(userInput) == 0:
+			print 'Please enter at least 1 character!'
+		elif len(userInput) >1 and userInput.isalpha():
+			print 'Please enter only 1 character!'
+		else:
+			print 'Please enter only letters and no numbers'
+	return userInput.lower()
+
+# An object input file parser.
+def ObjectInformation(header):
+	h,m,s = [ float(i) for i in header["RA"].split(":") ]
+	ra = 15*(h + m/60.0 + s/3600.0)
+	d,m,s = [ float(i) for i in header["DEC"].split(":") ]
+	if d<0:
+		dec = (d + m/60.0 + s/3600.0)*-1.0
+	else:
+		dec = (d + m/60.0 + s/3600.0)
+	
+	time_string = header["DATE-OBS"] + " " + header["UTC-OBS"]
+	t = time.Time(time_string)
+	jd = t.jd
+
+	return ra, dec, jd
+	
+
+
+# Observatory Information
+def ObservatoryInformation(filename):
+	c = ConfigParser.ConfigParser()
+	c.read(filename)
+	longi = c.getfloat("ObservatoryInformation", "longitude")
+	lat = c.getfloat("ObservatoryInformation", "latitude")
+	alt = c.getfloat("ObservatoryInformation", "altitude")
+	
+	return longi, lat, alt
+
+# Curve determination parameters.
+def CurveParams(filename):
+	c = ConfigParser.ConfigParser()
+	c.read(filename)
+	
+	stepsize = c.getint("CurveParameters","stepsize")
+	nsum = c.getint("CurveParameters","nsum")
+	uppercut = c.getint("CurveParameters", "uppercut")
+	pixel_scale = c.getfloat("CurveParameters", "pixelscale")
+	speed_light = c.getfloat("CurveParameters","speedlight")
+
+	return stepsize, nsum, uppercut, pixel_scale, speed_light
