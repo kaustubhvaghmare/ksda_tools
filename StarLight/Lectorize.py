68ae79624d5a58bb92695936d9a195d6a81a4147
Kaustubh Vaghmare <kaustubh.waghmare@gmail.com>
2015-04-07 15:04:15 +0530

    Added a module to do error bar computation via simulation for Lector. Modified original Lector execution file to include error bar estimates.

--- StarLight/Lectorize.py
+++ StarLight/Lectorize.py
@@ -10,6 +10,7 @@ import sys
 from astropy.table import Table
 import os
 import numpy as np
+import numpy.random as nr
 
 # Parse command line arguments.
 try:
@@ -24,6 +25,7 @@ try:
 except:
 	print("You have not run DerotExtract utility on this file. Please do so before using this tool.")
 	sys.exit(3)
+
 
 def RunLector(specfile):
 	"""
@@ -45,11 +47,48 @@ def RunLector(specfile):
 	os.rename("temp.spec_LINE", spec+"_LINE")
 	os.rename("temp.spec_ROSE", spec+"_ROSE")
 
+# Define a function to determine error bars.
+def LectorError(specfile, sim=100):
+	"""
+	Input: Spectrum Astropy Table
+	Output: Errors on indices
+	"""
+	# Make lector configuration file.
+	lect_conf = open("lector.in", "w")
+	lect_conf.write("s\ntemp.spec\n0\nn")
+	lect_conf.close()
+	
+	no_bands = len(Table.read("BANDS", format="ascii", data_start=4, header_start=None))
+	sim_data = np.zeros( (sim, no_bands), dtype=float)
+
+	# Load the spectrum.
+	spectrum = Table.read(specfile, format="ascii")
+#	lec_spec = spectrum["col1", "col2"]
+	
+	for i in range(sim):
+
+		# Make realization.
+		noise = nr.normal(0,1,len(spectrum)) * spectrum["col3"]
+		realization = spectrum["col1", "col2"]
+		realization["col2"] = realization["col2"] + noise
+		realization.write("temp.spec", format="ascii")
+
+		# Run lector.
+		os.system("/home/kaustubh/Tools/Lector/lector < lector.in")
+
+		sim_data[i] = np.array(open("temp.spec_LINE").read().split()[1:-3], dtype=float)
+	
+	errors = np.std(sim_data, axis=0)
+	err_out = open(spec+"_LINE_ERR", "w")
+	err_out.write("temp.spec    ")
+	err_out.write("  ".join([str(i) for i in errors]))
+	err_out.close()
+
 
 # Loop over all 1-d spectra.
 for spec in aper_map["col1"]:
-	# Load spectrum.
 	print("Working on %s" % spec)
-	RunLector(spec)
+	RunLector(spec) # Runs Lector
+	LectorError(spec) # Determines error
 	
 # Done.
